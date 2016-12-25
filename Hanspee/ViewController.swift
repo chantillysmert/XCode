e42461d95b1fbdf1d07187c86416d57400fc22cf
Emily Huo <emilyhuo@zhaohuis-mbp.home>
2016-12-25 09:44:20 -0500

    Initial Commit

---
+++ Hanspee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hanspee
+//
+//  Created by Emily Huo on 12/25/16.
+//  Copyright © 2016 Smert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
